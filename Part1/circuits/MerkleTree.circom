72f1dd2f79d7582da5aefb5b3715c115d6cbfacf
martonp <marci93@gmail.com>
2022-12-12 16:57:47 -0500

    Start.

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -5,15 +5,67 @@ include "../node_modules/circomlib/circuits/poseidon.circom";
 template CheckRoot(n) { // compute the root of a MerkleTree of n Levels 
     signal input leaves[2**n];
     signal output root;
+    signal levels[n-1][2**(n-1)];
 
-    //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    var numHashes = 0;
+    for (var i = 0; i < n; i++){
+        numHashes = 2**i + numHashes;
+    }
+    component hashes[numHashes];
+    var hashNum = 0;
+
+    for (var i = 0; i < 2**(n-1); i++) {
+        hashes[hashNum] = Poseidon(2);
+        hashes[hashNum].inputs[0] <== leaves[i * 2];
+        hashes[hashNum].inputs[1] <== leaves[i * 2 + 1];
+        levels[0][i] <== hashes[hashNum].out;
+        hashNum++;
+    }
+
+    for (var level = 1; level < n-1; level++) {
+        for (var i = 0; i < 2**level; i++) {
+            hashes[hashNum] = Poseidon(2);
+            hashes[hashNum].inputs[0] <== levels[level - 1][i * 2];
+            hashes[hashNum].inputs[1] <== levels[level - 1][i * 2 + 1];
+            levels[level][i] <== hashes[hashNum].out;
+            hashNum++;
+        }
+    }
+
+    root <== levels[0][0];
+}
+
+template DualMux() {
+    signal input in[2];
+    signal input s;
+    signal output out[2];
+
+    s * (1 - s) === 0;
+    out[0] <== (in[1] - in[0])*s + in[0];
+    out[1] <== (in[0] - in[1])*s + in[1];
 }
 
 template MerkleTreeInclusionProof(n) {
     signal input leaf;
     signal input path_elements[n];
     signal input path_index[n]; // path index are 0's and 1's indicating whether the current element is on the left or right
+
     signal output root; // note that this is an OUTPUT signal
 
-    //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    component selectors[n];
+    component hashes[n];
+
+    for (var i = 0; i < n; i++) {
+        selectors[i] = DualMux();
+        hashes[i] = Poseidon(2);
+
+        selectors[i].in[0] <== i == 0 ? leaf : hashes[i-1].out;
+        selectors[i].in[1] <== path_elements[i];
+        selectors[i].s <== path_index[i];
+
+        hashes[i].inputs[0] <== selectors[i].out[0];
+        hashes[i].inputs[1] <== selectors[i].out[1];
+    }
+
+    root <== hashes[n-1].out;
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
+import "hardhat/console.sol";
 
 contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
@@ -10,11 +11,46 @@ contract MerkleTree is Verifier {
     uint256 public root; // the current Merkle root
 
     constructor() {
-        // [assignment] initialize a Merkle tree of 8 with blank leaves
+        for (uint i = 0; i < 8; i++) {
+            hashes.push(0);
+        }
+
+        uint count = 8;
+        uint offset = 0;
+
+        while (count > 0) {
+            for (uint i = 0; i < count - 1; i+=2) {
+                uint256[2] memory toHash = [hashes[offset + i], hashes[offset + i + 1]];
+                hashes.push(PoseidonT3.poseidon(toHash));
+            }
+            offset = offset + count;
+            count = count / 2;
+        }
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
-        // [assignment] insert a hashed leaf into the Merkle tree
+        hashes[index] = hashedLeaf;
+        uint offset = 0;
+        uint count = 8;
+        uint256 previousIndex = index;
+
+        console.log(hashes.length);
+        while (count > 1) {
+            console.log(previousIndex);
+            uint256[2] memory toHash;
+            if (previousIndex % 2 == 0) {
+                toHash = [hashes[previousIndex], hashes[previousIndex + 1]];
+            } else {
+                toHash = [hashes[previousIndex - 1], hashes[previousIndex]];
+            }
+            previousIndex = offset + count + (previousIndex - offset) / 2;
+            hashes[previousIndex] = PoseidonT3.poseidon(toHash);
+            offset += count;
+            count = count / 2;
+        }
+
+        index++;
+        return hashes[14];
     }
 
     function verify(
@@ -23,7 +59,6 @@ contract MerkleTree is Verifier {
             uint[2] memory c,
             uint[1] memory input
         ) public view returns (bool) {
-
-        // [assignment] verify an inclusion proof and check that the proof root matches current root
+            return Verifier.verifyProof(a, b, c, input);
     }
 }
